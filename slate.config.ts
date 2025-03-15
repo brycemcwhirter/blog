6df59980d6e33a4c6b0be7be0541f29da973707d
Bryce McWhirter <bryce.mcwhirter@paycomonline.com>
2025-03-15 18:45:11 +0100

    Initial commit with custom blog template and small changes.

---
+++ slate.config.ts
@@ -0,0 +1,23 @@
+/*
+ * @file Theme configuration
+ */
+import { defineConfig } from './src/helpers/config-helper';
+
+export default defineConfig({
+  lang: 'en-US',
+  site: 'https://slate-blog-demo.vercel.app',
+  avatar: '/avatar.png',
+  title: 'Bryce McWhirter Blog',
+  description: 'A blog by Bryce McWhirter',
+  lastModified: true,
+  readTime: true,
+  footer: {
+    copyright: '© 2025 Slate Design',
+  },
+  socialLinks: [
+    {
+      icon: 'github',
+      link: 'https://github.com/SlateDesign/slate-blog',
+    },
+  ],
+});
